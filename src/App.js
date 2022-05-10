78e28732cb7b1b9d37acb4f8de293748babaa645
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-10 23:15:09 +0300

    taks 16

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Tours from "./components/tours/Tours";
+
+const toursData = require("./data/db.json");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      toursData.map(Tdata => {
+        return (
+          <Tours id={Tdata.id} name={Tdata.name} info={Tdata.info} price={Tdata.price} image={Tdata.image}/>
+        )
+      })
+    }
+    
+
+    <Footer/>
+    <Header/>
+    <Home/>
+    </>
+
+  
   );
 }
 
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props){
+return(
+    <>
+    <a href= "www.facebook.com"> Our facebook </a>
+     <p> Ahmad Amaireh and Mereen Mryan </p>
+    </>
+);
+}
+export default Footer;
