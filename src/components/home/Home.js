17e1c510242348c841e62b0f42a9da3d4ce2c8b9
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-14 14:38:32 +0300

    after edit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,25 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import Tours from "../tours/Tours";
+import toursData from "/home/ahmad/Travel-Destination/travel/src/data/db.json";
+
+
+// const toursData = require("./data/db.json");
+
+
 
 function Home (props){
 
     return(
         <>
+        {
+      toursData.map(Tdata => {
+        return (
+          <Tours id={Tdata.id} name={Tdata.name} info={Tdata.info} price={Tdata.price} image={Tdata.image}/>
+        )
+      })
+    }
+    
 
      <Header></Header>
      <Tours></Tours>
