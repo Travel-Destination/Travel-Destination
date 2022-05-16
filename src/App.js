4973cdbbf174cba790ea767c838a32549eef477f
mereenmryan <mereenmryan98@gmail.com>
2022-05-16 23:40:57 +0300

    new branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
-import Home from "./components/home/Home";
-import Tours from "./components/tours/Tours";
-import toursData from "./data/db.json";
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+// import Home from "./components/home/Home";
+// //import toursData from "/home/mereen/Travel-Destination/travel/Travel-Destination/src/data/db";
 // const toursData = require("./data/db.json");
+
+// function App() {
+//   return (
+//     <>
+//     <Header/>
+//     <Home toursData = {toursData}/>
+//     <Footer/>
+    
+//     </>
+
+  
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './components/home/Home.js';
+//import TourDetails from './components/TourDetails/TourDetails';
 
 function App() {
   return (
     <>
-
-{
-      toursData.map(Tdata => {
-        return (
-          <Tours id={Tdata.id} name={Tdata.name} info={Tdata.info} price={Tdata.price} image={Tdata.image}/>
-        )
-      })
-    }
-    <Home/>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      {/* <Route path="/city/:id" element={<TourDetails />} /> */}
+    </Routes>
     </>
-
-  
   );
 }
 
